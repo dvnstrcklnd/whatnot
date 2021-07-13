269b9b348e54baf7946088d1ec8e067d088d931c
Devin Strickland <strcklnd@uw.edu>
2021-07-13 09:46:39 -0400

    git setup

---
+++ git_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.st status
+git config --global alias.tree 'log --all --decorate --oneline --graph'
+git config --global user.name "Devin Strickland"
+git config --global user.email "strcklnd@uw.edu"
